f9a3054449fdf34745d4b9a1836539d6c2539b58
Jamal Gardiner <jgardiner@neat.com>
2021-03-13 21:27:42 -0500

    Initial Commit

---
+++ hw1.hs
@@ -0,0 +1,24 @@
+-- Double the value of every second digit beginning from the right.That is, the last digit is unchanged; the second-to-last digit is
+-- doubled; the third-to-last digit is unchanged; and so on. For example,[1,3,8,6]becomes[2,3,16,6].
+
+toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits x = toDigits (div x 10) ++ [mod x 10]
+
+doubleEverySecond :: [Integer] -> [Integer]
+doubleEverySecond [] = []
+doubleEverySecond [y] = [y]
+doubleEverySecond (x : y : xs) = [x * 2, y] ++ doubleEverySecond xs
+
+digs :: Integer -> Integer
+digs 0 = 0
+digs x = digs (div x 10) + mod x 10
+
+addDigits :: [Integer] -> Integer
+addDigits [] = 0
+addDigits (x : xs) = digs x + addDigits xs
+
+validate :: (Integral a) => a -> Bool
+validate x
+  | rem x 10 == 0 = True
+  | otherwise = False
